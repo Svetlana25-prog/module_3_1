23dd75c3191db77814f1f0997f4baa36b9c5a20f
Svetlana <skyfall12@mail.ru>
2024-10-21 17:51:33 +0300

    module_3_1

---
+++ main.py
@@ -0,0 +1,24 @@
+calls = 0
+def count_calls():
+    global calls
+    calls += 1
+
+def string_info(string):
+    count_calls()
+    return (len(string), string.upper(), string.lower())
+
+def is_contains(string, list_to_search):
+    count_calls()
+    list_to_search_new = []
+    for i in list_to_search:
+        list_to_search_new.append(i.lower())
+    if string.lower() in list_to_search_new:
+        return True
+    return False
+
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+print(calls)
